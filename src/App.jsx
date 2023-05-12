18b37c22ee7d80ce87f319128ce3e94bdce8b55e
Jocelyn Bonvard <jbonvard@aol.fr>
2023-05-12 13:37:29 +0200

    reset search

--- src/App.jsx
+++ src/App.jsx
@@ -26,17 +26,19 @@ function App() {
         <SearchBar searchCharacter={searchCharacter}/>
 
         {searchTerm ? (
-          <button onClick={resetSearch} className="text-white bg-red-700 hover:red-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+          <button onClick={resetSearch} className="text-white mb-8 bg-red-700 hover:red-blue-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
             Reset Filter
           </button>
         ) : undefined}
 
         <section className='grid grid-cols-2 md:grid-cols-4 xl:grid-cols-5 gap-5'>
-          {characters?.map((character, index) => 
+          {characters ? characters?.map((character, index) => 
             (
                 <Card key={index} character={character}/>
               )
-          )}
+          ) : (<div>
+                <h2 className='text-red-600 text-2xl text-bold'>Character not found 🙄</h2>
+              </div>)}
         </section>
       </main>    
     </>
